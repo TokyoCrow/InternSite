2432c0049d8b87d6923244eff5afdb7198bb1cc0
Emil Hasanov <68460577+TokyoCrow@users.noreply.github.com>
2020-09-07 18:23:52 +0300

    Update events_controller.rb

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -62,7 +62,7 @@ class EventsController < ApplicationController
     arguments[:repeat_every_month] = repeat[3]
     arguments[:repeat_every_year] = repeat[4]
     arguments[:weekday] = time.wday
-    if @event
+    if @event?
       arguments[:id] = @event.id
     return arguments
   end
