5e75807381b667b016e338484018a233aa578ada
Emil Hasanov <68460577+TokyoCrow@users.noreply.github.com>
2020-09-07 03:44:54 +0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 	 'signup'     => 'users#new'
+  get	   'profile'    => 'users#edit'
+  get    'login'      => 'sessions#new'
+  post   'login'      => 'sessions#create'
+  delete 'logout'     => 'sessions#destroy'
+  get    'my_events'  => 'events#user_events'
+  resources :users
+  resources :events
+  resources :sessions
+  root 'pages#index'
 end
