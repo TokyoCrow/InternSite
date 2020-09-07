d6ec259c44ce32360195a81f2401f009c848f9af
Emil Hasanov <68460577+TokyoCrow@users.noreply.github.com>
2020-09-07 19:55:13 +0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 	 'signup'     => 'users#new'
   get	   'profile'    => 'users#edit'
+  put    'profile'    => 'users#update'
   get    'login'      => 'sessions#new'
   post   'login'      => 'sessions#create'
   delete 'logout'     => 'sessions#destroy'
